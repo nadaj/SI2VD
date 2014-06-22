0eb8c074cf0d1213e1da58794f3dccc7160cec1b
nadaj <nadajankovic2004@yahoo.com>
2014-06-22 17:10:09 +0200

    ENGLESKA VERZIJA

--- pozoristenaterazijama/js/kviz.js
+++ pozoristenaterazijama/js/kviz.js
@@ -27,9 +27,11 @@ function potvrdi() {
 }
 
 bootstrap_alert = function() {}
+
 bootstrap_alert.warning = function(message) {
             $('#alert_placeholder').html('<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
         }
+		
 bootstrap_alert.test = function(message) {
             $('#alert_placeholder').html('<div class="alert alert-success"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
         }
@@ -63,4 +65,31 @@ function resetuj() {
 	 document.pageForm.odg10[2].checked = "false";
 	 document.pageForm.odg10[3].checked = "false";
 	  $('#alert_placeholder').html("");
+}
+
+function potvrdien() {
+	var tacni = 0;
+	
+	if (document.pageForm.odg1.value == "" || document.pageForm.odg2.value == "" || document.pageForm.odg4.value == "" || document.pageForm.odg6.value == "") {
+		bootstrap_alert.warning("Please fill in the blanks!") ;
+	}
+	if (document.pageForm.odg1.value == "Belgrade comedy") tacni++;
+	if (document.pageForm.odg2.value == "Modern theatre" || document.pageForm.odg2.value == "Savremeno pozoriste") tacni++;
+	if (document.pageForm.odg4.value == "Anniversary") tacni++;
+	if (document.pageForm.odg6.value == "Ivan Bosiljcic" || document.pageForm.odg6.value == "Ivan Bosiljčić")		  		tacni++;
+	
+
+	if (document.pageForm.odg3[1].checked == true && document.pageForm.odg3[3].checked == true) tacni++;
+	
+	if (document.pageForm.odg5[2].checked == true) tacni++;
+	
+	if (document.pageForm.odg7[1].checked == true) tacni++;
+	
+	if (document.pageForm.odg8[0].checked == true) tacni++;
+	
+	if (document.pageForm.odg9[0].checked == true) tacni++;
+	
+	if (document.pageForm.odg10[3].checked == true) tacni++;
+	
+	bootstrap_alert.test("*** Congratulations! You have " + tacni + " / 10 ***") ;
 }
