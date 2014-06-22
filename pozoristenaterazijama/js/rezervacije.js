0328b24af0937207d36e51d7c0cd36bfce51f517
Di-mi <ixydimitrov@gmail.com>
2014-06-22 04:10:09 +0200

    rezervacije.js

---
+++ pozoristenaterazijama/js/rezervacije.js
@@ -0,0 +1,18 @@
+String datum 	 = "";
+String vreme 	 = "";
+String predstava = "";
+String sala 	 = "";
+function klik(dat,vre,pred,sal){
+	datum 		= dat;
+	vreme 		= vre;
+	predstava	= pred;
+	sala 		= sal;
+}
+
+function popuni(){
+	document.pageForm.odg1.value = datum;
+	 document.pageForm.odg2.value = vreme;
+	 document.pageForm.odg3.value = predstava;
+	 document.pageForm.odg4.value = sala;
+
+}
